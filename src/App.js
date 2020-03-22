c5d564ced7ebdbb2b17b64daea3ed7f2bde9b9c2
imane <benbenimane@hotmail.com>
2020-03-22 21:44:39 +0100

    learned about states, omg that is so cool

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Hello from "./sayHello";
 import Tweet from "./tweet";
 function App() {
 
+ const [isRed,setRed] = useState(false);
+ const [count, setCount] =  useState(0);
 
+ const increment =() =>
+ {
+     setCount(count+1);
+ };
   return(
       <div className={"app"}>
 
         <div>
             <Hello/>
-            <Tweet  Name = "imane"/>
-            <Tweet  Name = "john"/>
-            <Tweet  Name = "hihi"/>
+            <button onClick={increment}>Increment</button>
+            <h1>{count}</h1>
+            <Tweet  Name = "Imane"/>
+            <Tweet  Name = "John"/>
+            <Tweet  Name = "Jef"/>
         </div>
 
       </div>
--- src/tweet.js
+++ src/tweet.js
@@ -1,11 +1,11 @@
 import React from "react";
 import './App.css';
-function Tweet(props) {
+function Tweet({Name}) {
 
     return(
 <div className={"tweet"}>
       <div>
-          <h3>{props.Name}</h3>
+          <h3>{Name}</h3>
           <p>tweet message goes here</p>
           <h3>number of likes</h3>
       </div>
